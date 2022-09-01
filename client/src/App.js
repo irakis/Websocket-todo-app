7130fcd323d04f7a78180bc944f036a7b6ed2364
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-09-01 21:11:15 +0200

    local client state not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,56 @@
 import './index.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+import shortid from 'shortid';
+import e from 'cors';
 
 const App = () => {
+
+  const [socket, setSocket] = useState(null);
+  const [tasks, setTask] = useState([]);
+  const [taskName, setTaskName] = useState([]);
+
+  console.log('tasks array', tasks);
+
+  useEffect(() => {
+    const socket = io.connect('http://localhost:8080', {
+      transports: ['websocket'],
+    })
+    setSocket(socket);
+
+    socket.on('updateData', (tasks) => updateTask(tasks));
+    socket.on('addTask', (task) => addTask(task));
+    socket.on('removeTask', (taskId) => removeTask(taskId));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const updateTask = (tasks) => {
+    setTask(tasks);
+  }
+
+  const removeTask = (e, taskId, local) => {
+    console.log('taskId w remove: ',taskId, local)
+    setTask((tasks) => tasks.filter(task => task.id !== taskId));
+    if (local === true)  socket.emit('removeTask', taskId );
+  }
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    
+    const newTask = { name: taskName, id: shortid() }
+    if(newTask.name == '') { alert ('Field cant be empty...') 
+      } else {
+    addTask(newTask)
+
+    socket.emit('addTask', newTask);
+    console.log('newTask w app:', newTask)
+    }
+  }
+
+  const addTask = ({ id, name }) => { setTask([...tasks, { id, name }])
+    console.log('taclica lokalna w app po add task?? ', tasks);
+  }
+
   return (
     <div className="App">
       <header>
@@ -11,16 +61,20 @@ const App = () => {
         <h2>Tasks</h2>
 
         <ul className="tasks-section__list" id="tasks-list">
-          <li className="task">Shopping <button className="btn btn--red">Remove</button></li>
-          <li className="task">Go out with a dog <button className="btn btn--red">Remove</button></li>
+          {console.log('tasks w html:', tasks)}
+          {tasks.map((item) => (<li key={shortid()} className="task">{item.name}
+              <button onClick={() => removeTask(e, item.id, true)} className="btn btn--red">Remove</button></li>))
+          }
+
         </ul>
 
-      <form id="add-task-form">
-        <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-        <button className="btn" type="submit">Add</button>
-      </form>
+        <form id="add-task-form" onSubmit={submitForm}>
+          <input className="text-input" autoComplete="off" value={taskName} type="text" placeholder="Type your description"
+            id="task-name" onChange={e => setTaskName(e.target.value)} />
+          <button className="btn" type="submit">Add</button>
+        </form>
 
-    </section>
+      </section>
     </div>
   );
 }
